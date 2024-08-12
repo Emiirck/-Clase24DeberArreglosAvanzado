df4685063ec1fc7d182757177cc397e3cd3b9cfc
Emiirck <emirck.candelo@tecnologicoliceocristiano.edu.ec>
2024-08-11 22:07:19 -0500

    Add files via upload

---
+++ cuentaletra.py
@@ -0,0 +1,9 @@
+
+
+palabras = ["manzana", "banana", "naranja", "manzana", "pera", "manzana", "uva"]
+
+palabra_especifica = "manzana"
+
+conteo = palabras.count(palabra_especifica)
+
+print(f"La palabra '{palabra_especifica}' aparece {conteo} veces en el arreglo.")
